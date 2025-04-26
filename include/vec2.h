6bb3577ab29eadceef0a55705a8ef86f08f2d39a
SebastianPaulus <svpaulus@gmail.com>
2025-04-25 19:57:37 -0500

    setup vec2.h

--- include/vec2.h
+++ include/vec2.h
@@ -3,5 +3,26 @@
 #ifndef VEC2_H
 #define VEC2_H
 
+#include <iostream>
+#include <cmath>
+
+class ValueError
+{};
+
+template< typename T >
+class vec2
+{
+public:
+	vec2(T x = 0, T y = 0)
+		: x_(x), y_(y)
+	{
+	}
+private:
+	T x_, y_;
+};
+
+typedef vec2< int > vec2i;
+typedef vec2< float > vec2f;
+typedef vec2< double > vec2d;
 
 #endif
